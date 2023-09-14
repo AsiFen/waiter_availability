3602ad376b24d228e32ce3f97a828c6bce6a6771
Asisipho <missfente@gmail.com>
2023-09-14 15:22:06 +0200

    separated routes

--- db/db_connect.js
+++ db/db_connect.js
@@ -13,9 +13,3 @@ const db = pgPromise()(connectPromise);
 db.connect();
 //export the database
 export default db;
-
-// if (process.env.DATABASE_URL) {
-//     console.log('DATABASE_URL is set:' + connectPromise.connectionString);
-//   } else {
-//     console.log('DATABASE_URL is not set.');
-//   }
